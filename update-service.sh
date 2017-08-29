5b405aed34ef6f102ad2d10a217789a158971466
root <root@ip-172-31-14-79.ec2.internal>
2017-08-29 15:55:58 +0000

    update service script file

--- update-service.sh
+++ update-service.sh
@@ -21,6 +21,7 @@ sed -e "s;%BUILD_NUMBER%;${BUILD_NUMBER};g" -e "s;%REPOSITORY_URI%;${REPOSITORY_
 chmod 777 pipelineFile-v_${BUILD_NUMBER}.json
 
 #Register the task definition in the repository
+
 aws ecs register-task-definition --family ${FAMILY} --cli-input-json file://pipelineFile-v_${BUILD_NUMBER}.json --region ${REGION}
 
 SERVICES=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER} --region ${REGION} | jq .failures[]`
